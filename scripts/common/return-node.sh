7ae15308f5e51c4b0aa451f63d0d89b4346be824
Niels de Vos <ndevos@redhat.com>
2018-07-29 11:58:09 +0200

    set CICO_API_KEY

--- scripts/common/return-node.sh
+++ scripts/common/return-node.sh
@@ -1,5 +1,8 @@
 # cico-node-done-from-ansible.sh
 # A script that releases nodes from a SSID file written by
+
+export CICO_API_KEY="$(<~/duffy.key)"
+
 SSID_FILE=${SSID_FILE:-$WORKSPACE/cico-ssid}
 
 for ssid in $(cat ${SSID_FILE})
